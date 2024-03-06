ab39f2d3318af07087fa354ff9d1dadc370424cd
LaurieGale10 <60512477+LaurieGale10@users.noreply.github.com>
2024-03-06 16:05:02 +0000

    Added exercises directory

---
+++ exercises/original/exercise_spare_c_original.py
@@ -0,0 +1,10 @@
+#This program is meant to add a certain value to a pair of coordinates. It inputs an x and y coordinate from the user, and then inputs a factor to multiply these by. It then prints the new coordinates. Fix the errors so that the correct output is printed.
+
+int(input("Enter an x coordinate")) = x
+int(input("Enter a y coordinate")) = y
+int(input("Enter a number to increase these coordinates by")) = factor
+
+x = x * factor
+y = y * factar
+print("The new coordinate is:")
+print("(x,y)")
